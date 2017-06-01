3c844c83603ad78fe6fc82927df656bb73a10d68
Shihlin Lu <shihlinlu@gmail.com>
2017-06-01 12:05:54 -0600

    added config file for slack

--- bots.php
+++ bots.php
@@ -1,10 +1,17 @@
 <?php
 require_once('vendor/autoload.php');
 
+//grab mySQL statement
+$config = readConfig("/etc/apache2/capstone-mysql/piomirrors.ini");
+
+//variable that will house the API key for the Slack API
+$slack = $config["slack"];
+
+//connects to Slack API
 $loop = ReactEventLoopFactory::create();
 
 $client = new SlackRealTimeClient($loop);
-$client->setToken('slack api token');
+$client->setToken($slack);
 $client->connect();
 
 $loop->run();
